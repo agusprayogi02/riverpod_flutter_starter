35e3a5134f580578a80c695f996517fdc4b92b52
Agus Prayogi <agus21apy@gmail.com>
2025-08-01 21:28:37 +0700

    [ADD] generate colors assets

--- lib/gen/assets.gen.dart
+++ lib/gen/assets.gen.dart
@@ -14,6 +14,16 @@ import 'package:flutter/widgets.dart';
 import 'package:flutter_svg/flutter_svg.dart' as _svg;
 import 'package:vector_graphics/vector_graphics.dart' as _vg;
 
+class $AssetsColorGen {
+  const $AssetsColorGen();
+
+  /// File path: assets/color/colors.xml
+  String get colors => 'assets/color/colors.xml';
+
+  /// List of all assets
+  List<String> get values => [colors];
+}
+
 class $AssetsIconsGen {
   const $AssetsIconsGen();
 
@@ -43,6 +53,7 @@ class $AssetsImagesGen {
 class Assets {
   const Assets._();
 
+  static const $AssetsColorGen color = $AssetsColorGen();
   static const $AssetsIconsGen icons = $AssetsIconsGen();
   static const $AssetsImagesGen images = $AssetsImagesGen();
 }
---
+++ lib/gen/colors.gen.dart
@@ -0,0 +1,124 @@
+// dart format width=80
+/// GENERATED CODE - DO NOT MODIFY BY HAND
+/// *****************************************************
+///  FlutterGen
+/// *****************************************************
+
+// coverage:ignore-file
+// ignore_for_file: type=lint
+// ignore_for_file: deprecated_member_use,directives_ordering,implicit_dynamic_list_literal,unnecessary_import
+
+import 'package:flutter/painting.dart';
+import 'package:flutter/material.dart';
+
+class MyColors {
+  MyColors._();
+
+  /// Color: #000000
+  static const Color black = Color(0xFF000000);
+
+  /// MaterialColor:
+  ///   50: #FFF7E4E4
+  ///   100: #FFEABABA
+  ///   200: #FFDD8D8D
+  ///   300: #FFCF5F5F
+  ///   400: #FFC43C3C
+  ///   500: #FFBA1A1A
+  ///   600: #FFB31717
+  ///   700: #FFAB1313
+  ///   800: #FFA30F0F
+  ///   900: #FF940808
+  static const MaterialColor danger = MaterialColor(
+    0xFFBA1A1A,
+    <int, Color>{
+      50: Color(0xFFF7E4E4),
+      100: Color(0xFFEABABA),
+      200: Color(0xFFDD8D8D),
+      300: Color(0xFFCF5F5F),
+      400: Color(0xFFC43C3C),
+      500: Color(0xFFBA1A1A),
+      600: Color(0xFFB31717),
+      700: Color(0xFFAB1313),
+      800: Color(0xFFA30F0F),
+      900: Color(0xFF940808),
+    },
+  );
+
+  /// Color: #979797
+  static const Color gray410 = Color(0xFF979797);
+
+  /// Color: #EEEEEE
+  static const Color gray70 = Color(0xFFEEEEEE);
+
+  /// MaterialColor:
+  ///   50: #FFE0EEE5
+  ///   100: #FFB3D4BE
+  ///   200: #FF80B792
+  ///   300: #FF4D9A66
+  ///   400: #FF268446
+  ///   500: #FF006E25
+  ///   600: #FF006621
+  ///   700: #FF005B1B
+  ///   800: #FF005116
+  ///   900: #FF003F0D
+  static const MaterialColor green = MaterialColor(
+    0xFF006E25,
+    <int, Color>{
+      50: Color(0xFFE0EEE5),
+      100: Color(0xFFB3D4BE),
+      200: Color(0xFF80B792),
+      300: Color(0xFF4D9A66),
+      400: Color(0xFF268446),
+      500: Color(0xFF006E25),
+      600: Color(0xFF006621),
+      700: Color(0xFF005B1B),
+      800: Color(0xFF005116),
+      900: Color(0xFF003F0D),
+    },
+  );
+
+  /// Color: #FFFFFF
+  static const Color white = Color(0xFFFFFFFF);
+
+  /// MaterialColor:
+  ///   50: #FFFFFCE4
+  ///   100: #FFFEF6BC
+  ///   200: #FFFEF190
+  ///   300: #FFFEEB64
+  ///   400: #FFFDE642
+  ///   500: #FFFDE221
+  ///   600: #FFFDDF1D
+  ///   700: #FFFCDA18
+  ///   800: #FFFCD614
+  ///   900: #FFFCCF0B
+  static const MaterialColor yellowOcher = MaterialColor(
+    0xFFFDE221,
+    <int, Color>{
+      50: Color(0xFFFFFCE4),
+      100: Color(0xFFFEF6BC),
+      200: Color(0xFFFEF190),
+      300: Color(0xFFFEEB64),
+      400: Color(0xFFFDE642),
+      500: Color(0xFFFDE221),
+      600: Color(0xFFFDDF1D),
+      700: Color(0xFFFCDA18),
+      800: Color(0xFFFCD614),
+      900: Color(0xFFFCCF0B),
+    },
+  );
+
+  /// MaterialAccentColor:
+  ///   100: #FFFFFFFF
+  ///   200: #FFFFEFD1
+  ///   400: #FFFFE6B7
+  ///   700: #FFFFE1A8
+  static const MaterialAccentColor yellowOcherAccent = MaterialAccentColor(
+    0xFFFFEFD1,
+    <int, Color>{
+      100: Color(0xFFFFFFFF),
+      200: Color(0xFFFFEFD1),
+      400: Color(0xFFFFE6B7),
+      700: Color(0xFFFFE1A8),
+    },
+  );
+}
